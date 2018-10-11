cead327ec1b85bea67ea869b698d7d7db1005453
ArifFirdausNasapola <43140658+ArifFirdausNasapola@users.noreply.github.com>
2018-10-11 22:34:54 +0700

    Add files via upload

---
+++ TA_webdas/update_post.php
@@ -0,0 +1,11 @@
+<?php 
+session_start();
+require_once("db.php");
+$id = $_POST["id"]; $judul = $_POST["judul"]; $isi = $_POST["isi"];
+$sql = "UPDATE posting SET judul='$judul', isi='$isi' WHERE id=$id";
+if (mysqli_query($conn, $sql)) {
+header("Location: daftarposting.php");
+}else {
+echo "Gagal" . $sql . "<br>" . mysqli_error($conn);
+}
+?>
